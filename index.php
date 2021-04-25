eff24508fb943571264c77511f1668826f368326
tbatlle <thierry.education81@gmail.com>
2021-04-25 18:17:02 +0200

    Nouvelle version d'index

--- index.php
+++ index.php
@@ -8,7 +8,7 @@ $_SESSION['role'] = 'administrateur';
 $title = "Page d'accueil";
 require 'elements/header.php'; 
 ?>
-
+<h1>Test GIT</h1>
 <div class="starter-template">
   <h1>Bootstrap starter template</h1>
   <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
